c83de8a22f2549300cf6ddd0f1e056855f3cb34c
宮崎 洋史 <h@myzk.me>
2015-12-14 16:00:00 +0900

    マイグレーションファイルに、新しいカラムを定義した

--- db/migrate/20151214065807_add_columus_to_users.rb
+++ db/migrate/20151214065807_add_columus_to_users.rb
@@ -1,4 +1,9 @@
 class AddColumusToUsers < ActiveRecord::Migration
   def change
+    add_column :users, :name, :string
+    add_column :users, :organization, :string
+    add_column :users, :profile, :text
+    add_column :users, :works, :string
+    add_column :users, :photo, :text
   end
 end
