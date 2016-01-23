6b1cf9ea6bc4843382536ddeee54350149ea6723
宮崎 洋史 <h@myzk.me>
2016-01-23 19:28:00 +0900

    comment, like, tagというモデルとそのテーブルを作成した

---
+++ db/migrate/20160123100801_create_tags.rb
@@ -0,0 +1,10 @@
+class CreateTags < ActiveRecord::Migration
+  def change
+    create_table :tags do |t|
+      t.string :tag_name
+      t.integer :prototype_id
+
+      t.timestamps null: false
+    end
+  end
+end
